f63849e2b4c240c38bcec36efd5a9014b1bf0b0a
Juanita\Lukas Roberson <lukeroberson24@yahoo.com>
2021-08-17 11:53:58 -0400

    Working weather api

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,28 @@
-const settings = {
-	"async": true,
-	"crossDomain": true,
-	"url": "https://national-weather-service.p.rapidapi.com/points/%7Bpoint%7D/forecast/hourly",
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-key": "7b8728bf8cmsh5fe8553dacf422dp134527jsn15f30cf68ef9",
-		"x-rapidapi-host": "national-weather-service.p.rapidapi.com"
-	}
-};
+var buttonEl = document.querySelector('#btn')
+var inputValue = document.querySelector('.form-control')
+var cityName = document.querySelector('.name')
+var desc = document.querySelector('.desc')
+var temp = document.querySelector('.temp')
 
-$.ajax(settings).done(function (response) {
-	console.log(response);
-});
+// console.log(ButtonEl)
+
+buttonEl.addEventListener('click', function (event) {
+    event.preventDefault()
+    console.log(inputValue.value)
+    var url = 'https://api.openweathermap.org/data/2.5/weather?q=' + inputValue.value + '&appid=47d402ab8a0a4f0f7b9c25f732362d6b';
+    console.log(url)
+
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            var nameValue = data['name'];
+            var tempValue = data['main']['temp'];
+            var descValue = data['weather'][0]['description'];
+
+            cityName.innerHTML = nameValue;
+            temp.innerHTML = tempValue;
+            desc.innerHTML = descValue;
+        })
+        .catch(err => alert("Wrong city name!"))
+})
+
