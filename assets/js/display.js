688c7b0ccd8003d663952d6c8d82e6ff9803f1d2
Kevin LaCarrubba <Kevin.LaCarrubba@yahoo.com>
2021-08-18 13:45:27 -0400

    saved to local storage. made display js. getitems. set up data in consoleLog

---
+++ assets/js/display.js
@@ -0,0 +1,64 @@
+function yelpAPI() {
+  var cityName = localStorage.getItem("City Name");
+  var yelpURL =
+    "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?location=" +
+    cityName;
+  var yelpAPIkey =
+    "9etXOwMgbWmpRTmXKBVO5IkljuKPy3OeJNKr3CrznaGrwutFfd_tB8Vwgs9p8_jS4OTajW7iVVVWX40kdCULRCBg26xGgN0gwRK2AHzx1BTzt-zXOBu5t-8nwsIbYXYx";
+  fetch(yelpURL, {
+    headers: {
+      accept: "application/json",
+      "x-requested-with": "xmlhttprequest",
+      "Access-Control-Allow-Origin": "*",
+      Authorization: `Bearer ${yelpAPIkey}`,
+    },
+  })
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      console.log(data.businesses[0].name);
+      console.log(data.businesses[0].display_phone);
+      console.log(data.businesses[0].image_url);
+      console.log(data.businesses[0].location.display_address.join(", "));
+
+      //   console.log("---Top 5 bussiness name---");
+      //   var firstCardDisplay = document.getElementById("first-card");
+      //   console.log(firstCardDisplay);
+
+      //   for (var i = 0; i < 5; i++) {
+      //     firstCardDisplay.textContent = data.businesses[0].name.value;
+      //     console.log(data.businesses[i].name);
+      //   }
+    })
+    .catch(function (err) {
+      console.error(err);
+    });
+}
+
+//Weather API Function
+function weatherAPI() {
+  //Global Search Variables
+  var cityName = localStorage.getItem("City Name");
+  var weatherURL =
+    "https://api.weatherapi.com/v1/forecast.json?key=f3dd10a5b85d43bea96181956211608&q=" +
+    cityName +
+    "=5&aqi=no&alerts=no";
+  fetch(weatherURL)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      //   console.log("---City Name, State---");
+      //   console.log(data.location.name + ", " + data.location.region);
+      //   console.log("---Day's Avg Temp---");
+      //   console.log(data.forecast.forecastday[0].day.avgtemp_f);
+      //   console.log("---Day's Condition---");
+      //   console.log(data.current.condition.text);
+    });
+}
+
+yelpAPI();
+weatherAPI();
