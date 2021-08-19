cb21b28c527caf536403c5d68534b567226b30db
Kevin LaCarrubba <Kevin.LaCarrubba@yahoo.com>
2021-08-19 11:27:48 -0400

    created yelp carousel

--- assets/js/display.js
+++ assets/js/display.js
@@ -20,7 +20,7 @@ function yelpAPI() {
       return response.json();
     })
     .then(function (data) {
-      for (var i = 0; i < 5; i++) {
+      for (var i = 0; i < 9; i++) {
         var businessName = data.businesses[i].name;
         var displayPhone = data.businesses[i].display_phone;
         var image = data.businesses[i].image_url;
@@ -36,6 +36,9 @@ function yelpAPI() {
         };
         businessInfo.push(businessObj);
       }
+    })
+    .then(function () {
+      renderCard();
     })
     .catch(function (err) {
       console.error(err);
@@ -67,3 +70,130 @@ function weatherAPI() {
 
 yelpAPI();
 weatherAPI();
+
+// var businessInfo = [];
+// var businessObj = {
+//   name: businessName,
+//   phone: displayPhone,
+//   image: image,
+//   address: displayAddress,
+//   url: yelpURL,
+// };
+var viewOne = document.getElementById("view-one");
+var viewTwo = document.getElementById("view-two");
+var viewThree = document.getElementById("view-three");
+function renderCard() {
+  businessInfo.forEach((item, key) => {
+    console.log(item.name);
+    console.log(key);
+    //create the 3 columns
+    if (key == 0 || key == 1 || key == 2) {
+      var createCol = document.createElement("div");
+      createCol.classList.add("col-md-4", "my-3");
+      //create the card
+      var createCard = document.createElement("div");
+      createCard.classList.add("card");
+      //creata and link card img
+      var cardImg = document.createElement("img");
+      cardImg.src = item.image;
+      //create the card body
+      var createCardBody = document.createElement("card-body");
+      createCardBody.classList.add("card-body");
+      //create the card title and give it the business name
+      var createCardTitle = document.createElement("h4");
+      createCardTitle.classList.add("card-title");
+      createCardTitle.textContent = item.name;
+      //create an unordered list for phone and adddress
+      var infoList = document.createElement("ul");
+      var phoneLi = document.createElement("li");
+      var addressLi = document.createElement("li");
+      phoneLi.textContent = item.phone;
+      addressLi.textContent = item.address;
+      //append phone and address to list
+      infoList.appendChild(addressLi);
+      infoList.appendChild(phoneLi);
+      //append the title then the list to the body
+      createCardBody.appendChild(createCardTitle);
+      createCardBody.appendChild(infoList);
+      //append the image then the body to the card
+      createCard.appendChild(cardImg);
+      createCard.appendChild(createCardBody);
+      //append the card tot he col
+      createCol.appendChild(createCard);
+      //append the row to the view-one display
+      viewOne.appendChild(createCol);
+    }
+    if (key == 3 || key == 4 || key == 5) {
+      var createCol = document.createElement("div");
+      createCol.classList.add("col-md-4", "my-3");
+      //create the card
+      var createCard = document.createElement("div");
+      createCard.classList.add("card");
+      //creata and link card img
+      var cardImg = document.createElement("img");
+      cardImg.src = item.image;
+      //create the card body
+      var createCardBody = document.createElement("card-body");
+      createCardBody.classList.add("card-body");
+      //create the card title and give it the business name
+      var createCardTitle = document.createElement("h4");
+      createCardTitle.classList.add("card-title");
+      createCardTitle.textContent = item.name;
+      //create an unordered list for phone and adddress
+      var infoList = document.createElement("ul");
+      var phoneLi = document.createElement("li");
+      var addressLi = document.createElement("li");
+      phoneLi.textContent = item.phone;
+      addressLi.textContent = item.address;
+      //append phone and address to list
+      infoList.appendChild(addressLi);
+      infoList.appendChild(phoneLi);
+      //append the title then the list to the body
+      createCardBody.appendChild(createCardTitle);
+      createCardBody.appendChild(infoList);
+      //append the image then the body to the card
+      createCard.appendChild(cardImg);
+      createCard.appendChild(createCardBody);
+      //append the card tot he col
+      createCol.appendChild(createCard);
+      //append the row to the view-one display
+      viewTwo.appendChild(createCol);
+    }
+    if (key == 6 || key == 7 || key == 8) {
+      var createCol = document.createElement("div");
+      createCol.classList.add("col-md-4", "my-3");
+      //create the card
+      var createCard = document.createElement("div");
+      createCard.classList.add("card");
+      //creata and link card img
+      var cardImg = document.createElement("img");
+      cardImg.src = item.image;
+      //create the card body
+      var createCardBody = document.createElement("card-body");
+      createCardBody.classList.add("card-body");
+      //create the card title and give it the business name
+      var createCardTitle = document.createElement("h4");
+      createCardTitle.classList.add("card-title");
+      createCardTitle.textContent = item.name;
+      //create an unordered list for phone and adddress
+      var infoList = document.createElement("ul");
+      var phoneLi = document.createElement("li");
+      var addressLi = document.createElement("li");
+      phoneLi.textContent = item.phone;
+      addressLi.textContent = item.address;
+      //append phone and address to list
+      infoList.appendChild(addressLi);
+      infoList.appendChild(phoneLi);
+      //append the title then the list to the body
+      createCardBody.appendChild(createCardTitle);
+      createCardBody.appendChild(infoList);
+      //append the image then the body to the card
+      createCard.appendChild(cardImg);
+      createCard.appendChild(createCardBody);
+      //append the card tot he col
+      createCol.appendChild(createCard);
+      //append the row to the view-one display
+      viewThree.appendChild(createCol);
+    }
+  });
+}
